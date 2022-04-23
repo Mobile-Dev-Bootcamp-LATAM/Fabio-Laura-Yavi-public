1834ecf43d1f83c2356afc8c5173aad1ad638897
flaura <fabio.laura@coxautoinc.com>
2022-04-23 13:39:16 -0400

    style(CountryDetailView): Improving styles of the component

--- src/views/CountryDetailView.js
+++ src/views/CountryDetailView.js
@@ -1,75 +1,91 @@
 import { useContext, useEffect, useState } from "react";
 import styled from "styled-components";
 import { Flag } from "../atoms/Flag";
-import { Title } from '../atoms/Title';
+import { Title } from "../atoms/Title";
 import context from "../context/context";
-import { CurrencyConverter } from '../molecules/CurrencyConverter';
+import { CurrencyConverter } from "../molecules/CurrencyConverter";
 
 const getLat = (latlng) => {
-    return latlng.length >= 1 ? latlng[0] : 'Not specified';
-}
+  return latlng.length >= 1 ? latlng[0] : "Not specified";
+};
 
 const getLng = (latlng) => {
-    return latlng.length >= 2 ? latlng[1] : 'Not specified';
-}
+  return latlng.length >= 2 ? latlng[1] : "Not specified";
+};
 
 const getLanguage = (languages) => {
-    return languages.length > 0 ? languages[0].name : 'Not specified';
-}
+  return languages.length > 0 ? languages[0].name : "Not specified";
+};
 
 const getCurrency = (currencies) => {
-    return currencies.length > 0 ? `${currencies[0].code} ${currencies[0].symbol}` : 'Not specified';
-}
+  return currencies.length > 0
+    ? `${currencies[0].code} ${currencies[0].symbol}`
+    : "Not specified";
+};
 
 const Details = styled.div`
-    display: grid;
-    grid-template-columns: 1fr 1fr;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
 
-    label {
-        font-weight: bold;
-    }
+  label {
+    font-weight: bold;
+  }
 `;
 
 const DetailsCtn = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    gap: 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 2rem;
+`;
+
+const InnerCtn = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 2rem;
+  border: 1px solid gray;
+  border-radius: 5px;
+  padding: 1rem 1rem 5rem 1rem;
 `;
 
 const CountryDetailView = () => {
-    const reducer = useContext(context);
-    const { state, dispatch } = reducer;
-    const { selectedCountry, currentCountry } = state;
+  const reducer = useContext(context);
+  const { state, dispatch } = reducer;
+  const { selectedCountry, currentCountry } = state;
 
-    return (
-        <div>
-            <DetailsCtn>
-                <Title>{ selectedCountry.name }</Title>
-                <Flag src={ selectedCountry.flags.png} size={5}/>
-                <Details>
-                    <label>Capital</label>
-                    <span>{ selectedCountry.capital }</span>
-                    <label>Population</label>
-                    <span>{ selectedCountry.population }</span>
-                    <label>Language</label>
-                    <span>{ getLanguage(selectedCountry.languages)}</span>
-                    <label>Area</label>
-                    <span>{ selectedCountry.area }</span>
-                    <label>Lat</label>
-                    <span>{ getLat(selectedCountry.latlng) }</span>
-                    <label>Long</label>
-                    <span>{ getLng(selectedCountry.latlng) }</span>
-                    <label>Currency</label>
-                    <span>{ getCurrency(selectedCountry.currencies) }</span>
-                </Details>
-                <CurrencyConverter countryA={currentCountry} countryB={selectedCountry}/>
-            </DetailsCtn>
-        </div>
-    );
-}
+  return (
+    <div>
+      <DetailsCtn>
+        <Title>{selectedCountry.name}</Title>
+        <InnerCtn>
+          <Flag src={selectedCountry.flags.png} size={5} />
+          <Details>
+            <label>Capital</label>
+            <span>{selectedCountry.capital}</span>
+            <label>Population</label>
+            <span>{selectedCountry.population}</span>
+            <label>Language</label>
+            <span>{getLanguage(selectedCountry.languages)}</span>
+            <label>Area</label>
+            <span>{selectedCountry.area}</span>
+            <label>Lat</label>
+            <span>{getLat(selectedCountry.latlng)}</span>
+            <label>Long</label>
+            <span>{getLng(selectedCountry.latlng)}</span>
+            <label>Currency</label>
+            <span>{getCurrency(selectedCountry.currencies)}</span>
+          </Details>
+          <CurrencyConverter
+            countryA={currentCountry}
+            countryB={selectedCountry}
+          />
+        </InnerCtn>
+      </DetailsCtn>
+    </div>
+  );
+};
 
-export {
-    CountryDetailView
-}
+export { CountryDetailView };
